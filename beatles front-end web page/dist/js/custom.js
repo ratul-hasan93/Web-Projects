324fa4941de85f3254b74cd225581911b61bd0a0
Ratul Hasan Rafi <83693046+ratul-hasan93@users.noreply.github.com>
2022-10-22 16:42:54 +0100

    beatles web page commit done

---
+++ beatles front-end web page/dist/js/custom.js
@@ -0,0 +1,65 @@
+// $(document).ready(function(){
+
+// 	$navOffset = $('#menu_area').offset().top;
+            
+//     // navfix  start 
+//         $top_offset=300;
+//         $(window).scroll(function(){
+//             $scrolling=$(this).scrollTop();
+            
+//             if($scrolling > $navOffset){
+//                 $("#menu_area").addClass('navfixed');
+//             }else{
+//                 $("#menu_area").removeClass('navfixed');
+//             }
+//         // navfix  ends
+           
+//         });
+
+
+//         // slick slider
+//         $('.team_slide').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   // autoplay: true,
+//   // autoplaySpeed: 2000,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//     // You can unslick at a given breakpoint now by adding:
+//     // settings: "unslick"
+//     // instead of a settings object
+//   ]
+// });
+//         // slick slider
+
+//         // veno box
+
+//         $('.venobox').venobox(); 
+//         // veno box
+
+// });
